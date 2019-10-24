5553bcfde352512b321cd612a7dec24ceb4539db
Katie Rose <katiefelten@gmail.com>
2019-10-24 14:59:49 -0400

    :zap: connected to mongodb instance

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+mongodb+mongodb+srv://admin:passpass@cluster0-p2ap7.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
