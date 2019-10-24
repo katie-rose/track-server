7649899de9180775da035a0727889e559dfe6c87
Katie Rose <katiefelten@gmail.com>
2019-10-24 15:57:34 -0400

    :zap: users are being created and sent to mongo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri =
-  "mongodb+mongodb+mongodb+srv://admin:passpass@cluster0-p2ap7.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://admin:passpass@cluster0-p2ap7.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true,
